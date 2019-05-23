78d13087c7e7280a28d95e7af4f48da139744b21
1432214509 <1432214509@qq.com>
2019-05-23 16:26:28 +0800

    first commit

---
+++ school/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$("#a").click(function(){
+
+		$("#b").fadeIn(3000);	
+		
+	});
+});
---
+++ school/js/text.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	/*alert("123456");*/
+	$(".anniu").click(function(){
+		/*淡入效果*/
+		$(".header ul li").fadeIn(3000);
+		/*淡出效果*/
+		$(".header ul li").fadeOut(7000);
+		/*效果切换*/
+		/*$(".b").fadeToggle(3000);*/
+		/*获取值并弹出*/
+		/*alert( $(".input").val());*/
+		/*滑动*/
+		/*$(".b").slideDown();*/
+		/*$(".b").slideToggle();*/
+		/*动画*/
+		/*$(".header ul li").animate({left:'250px',opacity:'0.5',width:'-=30px',height:'-=30px'});*/
+		/*var a= $(".a");
+		a.animate({fontSize:'36px'},'fast');*/
+		
+	});
+});
